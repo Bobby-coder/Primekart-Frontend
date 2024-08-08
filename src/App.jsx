ee316c1683a0cab56ca164d21f9336cae944bddb
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-09 01:44:16 +0530

    Routing for all pages implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import CartPage from "./pages/CartPage";
+import WishlistPage from "./pages/WishlistPage";
+
 function App() {
   return (
-    <>
-      <p>Heyy!</p>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/products" element={<ProductsPage />}></Route>
+      <Route path="/products/:productId" element={<ProductDetailPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/wishlist" element={<WishlistPage />} />
+    </Routes>
   );
 }
 
