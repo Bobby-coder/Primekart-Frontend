7d4b6bc051bcb7fe2e5639bbe597acb5200091c4
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-09 01:44:55 +0530

    Homepage Page added

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+function HomePage() {
+  return (
+    <>
+      <p>HomePage</p>
+      <Button variant="outline">Button</Button>
+      <Button>Button</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="outline">Outline</Button>
+    </>
+  );
+}
+
+export default HomePage;
