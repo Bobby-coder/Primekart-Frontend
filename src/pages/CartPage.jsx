fce54204551f7271c9d42aa62cc00ce308aa38b1
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-09 01:41:33 +0530

    Cart Page UI implemented

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,13 @@
+import CartItemList from "@/components/cart/cartItemList";
+import SaveForLaterGrid from "@/components/saveForLater/SaveForLaterGrid";
+
+function CartPage() {
+  return (
+    <div className="flex flex-col gap-3 mx-auto lg:max-w-[980px] xl:max-w-[1220px]">
+      <CartItemList />
+      <SaveForLaterGrid />
+    </div>
+  );
+}
+
+export default CartPage;
