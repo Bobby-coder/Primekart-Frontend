2a6613833e6818bf9c665f186244592f661210b9
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-14 17:36:34 +0530

    Header and Footer components imported

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import ProductsPage from "./pages/ProductsPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import CartPage from "./pages/CartPage";
 import WishlistPage from "./pages/WishlistPage";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/products" element={<ProductsPage />}></Route>
-      <Route path="/products/:productId" element={<ProductDetailPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/wishlist" element={<WishlistPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/products" element={<ProductsPage />}></Route>
+        <Route path="/products/:productId" element={<ProductDetailPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
