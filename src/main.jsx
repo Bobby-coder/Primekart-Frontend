af9e8463e1297704faf43f814c97ec80cc3af182
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-31 01:50:51 +0530

    DND provider added

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,17 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import { Toaster } from "react-hot-toast";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <Toaster position="bottom-right" reverseOrder={true} />
-        <App />
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
