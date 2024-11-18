7b0adfecb19a259db2faf68a2ea13b2009cd825e
Bobby-coder <bobbysadhwani612@gmail.com>
2024-11-18 13:43:14 +0530

    Search Page bug fixed

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -15,20 +15,19 @@ function SearchPage() {
     fetchProducts(searchText);
   }, [searchText]);
 
+  // function to fetch products
   function fetchProducts(searchText) {
-    if (searchText) {
-      setLoading(true);
-      axios
-        .get(`https://dummyjson.com/products/search?q=${searchText}`)
-        .then((res) => {
-          setLoading(false);
-          setProducts(res.data.products);
-        })
-        .catch((err) => {
-          setLoading(false);
-          setError(err);
-        });
-    }
+    setLoading(true);
+    axios
+      .get(`https://dummyjson.com/products/search?q=${searchText}`)
+      .then((res) => {
+        setLoading(false);
+        setProducts(res.data.products);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err);
+      });
   }
 
   return (
@@ -39,35 +38,34 @@ function SearchPage() {
       )}
 
       {/*Search items grid*/}
-      {searchText && (
-        <div className="grid grid-cols-2 gap-2 p-2 lg:grid-cols-5">
-          {loading ? (
-            <p>Loading...</p>
-          ) : error ? (
-            <p>{error.data?.message || "Something went wrong"}</p>
-          ) : products.length === 0 ? (
-            // No products found UI
-            <div className="flex flex-col items-center justify-center col-span-full mt-10">
-              <img
-                src={notFoundImage}
-                alt="No results found"
-                className="w-40 h-40 mb-4"
-              />
-              <p className="text-lg font-semibold text-gray-600">
-                No products found
-              </p>
-              <p className="text-md text-gray-500">
-                Try searching with different keywords.
-              </p>
-            </div>
-          ) : (
-            // Render resultant products only if search text is not empty
-            products.map((product) => (
-              <ProductGridItem key={product.id} product={product} />
-            ))
-          )}
-        </div>
-      )}
+
+      <div className="grid grid-cols-2 gap-2 p-2 lg:grid-cols-5">
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error.data?.message || "Something went wrong"}</p>
+        ) : products.length === 0 ? (
+          // No products found UI
+          <div className="flex flex-col items-center justify-center col-span-full mt-10">
+            <img
+              src={notFoundImage}
+              alt="No results found"
+              className="w-40 h-40 mb-4"
+            />
+            <p className="text-lg font-semibold text-gray-600">
+              No products found
+            </p>
+            <p className="text-md text-gray-500">
+              Try searching with different keywords.
+            </p>
+          </div>
+        ) : (
+          // Render resultant products only if search text is not empty
+          products.map((product) => (
+            <ProductGridItem key={product.id} product={product} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
